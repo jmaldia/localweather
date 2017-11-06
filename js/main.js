4a18ace0f656919966a75f07a74a08a848345541
Jon Maldia <jon@maldia.net>
2017-11-05 21:52:46 -0500

    Adds functionality to convert celsius <=> fahrenheit. Edits on CSS Style and page Structure.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,75 @@
 $(document).ready(function() {
-
   // Get current position
+  var currTemp = '';
+  
   navigator.geolocation.getCurrentPosition(function(position) {
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
+   
+  var weatherObj = $.getJSON(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
         
   // This gets the weather details
-  $.getJSON(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`).done(update).fail(handleErr);
+  weatherObj.done(update).fail(handleErr);
+    
+  $('#button-convert').click(function() {
+      weatherObj.done(clickUpdate).fail(handleErr);
+    });
       });
-
-
+  
+  function clickUpdate(response) {
+    var tempJSON = JSON.stringify(response.main.temp);
+    
+    alert(currTemp + '<=>' + Math.round(tempJSON));
+    
+    if (currTemp === Math.round(tempJSON)) {
+      currTemp = Math.round((tempJSON * 1.8) + 32);
+      $('#weather').html('<h1 id="temperature">' + currTemp + '&deg;</h1>');
+      $('#buttons').html('<button id="button-convert">Convert to Celsius        </button>');
+      $('#details').html('<p>H: ' + Math.round((response.main.temp_max * 1.8) + 32) + '&deg; | L: ' + Math.round((response.main.temp_min * 1.8) + 32) + '&deg;</p>');
+      alert(currTemp);
+    } else {
+      currTemp = Math.round(tempJSON);
+      $('#weather').html('<h1 id="temperature">' + currTemp + '&deg;</h1>');
+      $('#buttons').html('<button id="button-convert">Convert to Fahrenheit        </button>');
+      $('#details').html('<p>H: ' + Math.round(response.main.temp_max) + '&deg; | L: ' + Math.round(response.main.temp_min) + '&deg;</p>');
+      alert(currTemp);
+    }
+  }
+  
   // Function to update the the weather
   function update(response) {
     var temp = JSON.stringify(response.main.temp);
-    var fahrenheit = Math.round((temp * 1.8) + 32);
+    currTemp = Math.round((temp * 1.8) + 32);
+    var high = Math.round((response.main.temp_max * 1.8) + 32);
+    var low = Math.round((response.main.temp_min * 1.8) + 32);
 
-    // Update the background video based on the current conditions
+    // Update the background video based on the current conditions   
     $('#weather-vid').attr('src', function() {
-      if (response.weather[0].main === 'Snow') {
-        return 'http://jonmaldia.com/tenCadenceWeather/img/snowy.mp4';
-      } else if (response.weather[0].main === 'Mist') {
-        return 'http://jonmaldia.com/tenCadenceWeather/img/cloudy.mp4';
-      } else if (response.weather[0].main === 'Rain') {
-        return 'http://jonmaldia.com/tenCadenceWeather/img/rainy.mp4';
-      } else {
-        return 'http://jonmaldia.com/tenCadenceWeather/img/sunny.mp4';
+      switch (response.weather[0].main) {
+        case 'Snow':
+          return 'http://jonmaldia.com/tenCadenceWeather/img/snowy.mp4';
+          break;
+        case 'Mist':
+          return 'http://jonmaldia.com/tenCadenceWeather/img/misty.mp4';
+          break;
+          case 'Cloudy':
+          return 'http://jonmaldia.com/tenCadenceWeather/img/cloudy.mp4';
+          break;
+        case 'Rain':
+          return 'http://jonmaldia.com/tenCadenceWeather/img/rainy.mp4';
+          break;
+        default:
+          return 'http://jonmaldia.com/tenCadenceWeather/img/sunny.mp4';
       }
-    });
-    
+    });   
     // Loads the video 
     $("#bg-vid")[0].load();
 
     // Updates the page temperature
-    $('#weather').html('<h1>' + fahrenheit + '&deg;</h1> <p>lat: ' + response.coord.lat + ' lon: ' + response.coord.lon + '<p> Current Conditions: ' + response.weather[0].main + '</p>');
+    $('#location').html('<h2>' + response.name + '</h2>');
+    $('#conditions').html('<h4>' + response.weather[0].main + '</h4>');
+    $('#weather').html('<h1 id="temperature">' + currTemp + '&deg;</h1>');
+    $('#details').html('<p>H: ' + high + '&deg; | L: ' + low + '&deg;</p>');
   }
 
   // Error handler
